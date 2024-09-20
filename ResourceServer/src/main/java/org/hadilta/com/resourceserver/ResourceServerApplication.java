08b725a13fa28d882dbf606a7cf6137dcd018ab9
hadi <hvahabpoor@gmail.com>
2024-09-20 16:11:31 +0300

    ResourceServer Config it to register in Eureka with basic auth.

--- ResourceServer/src/main/java/org/hadilta/com/resourceserver/ResourceServerApplication.java
+++ ResourceServer/src/main/java/org/hadilta/com/resourceserver/ResourceServerApplication.java
@@ -2,7 +2,9 @@ package org.hadilta.com.resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ResourceServerApplication {
 
